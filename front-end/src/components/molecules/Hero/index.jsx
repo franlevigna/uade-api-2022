8ff5d2283b2c3de63e8b78c1d0b61e14ca6a58fa
Marcos Rodriguez <mer.marcosrQgmail.com>
2022-09-06 00:00:39 -0300

    Home Layout

---
+++ front-end/src/components/molecules/Hero/index.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+
+export const Hero = () => {
+	return (
+		<Box
+			sx={{
+				bgcolor: 'background.paper',
+				pt: 8,
+				pb: 6,
+			}}
+		>
+			<Container maxWidth='sm'>
+				<Typography
+					component='h1'
+					variant='h2'
+					align='center'
+					color='text.primary'
+					gutterBottom
+				>
+					<img
+						height='200px'
+						alt='Profe flix'
+						src='\assets\profeFlix.svg'
+					></img>
+				</Typography>
+				<Typography
+					variant='h5'
+					align='center'
+					color='text.secondary'
+					paragraph
+				>
+					En esta aplicacion podras encontrar el profesor ideal, como
+					asi tambien a tu alumno ideal
+				</Typography>
+			</Container>
+		</Box>
+	);
+};
