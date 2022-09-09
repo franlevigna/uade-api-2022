a06a166cf7377b7e31e82f13da75210a54d8513a
Marcos Rodriguez <mer.marcosrQgmail.com>
2022-09-09 00:21:17 -0300

    Formatting and Linting

--- front-end/src/utils/Storage/index.js
+++ front-end/src/utils/Storage/index.js
@@ -1,26 +1,26 @@
 export const Storage = (key, shouldPersist = false) => {
-  const storage = shouldPersist ? window.localStorage : window.sessionStorage;
+	const storage = shouldPersist ? window.localStorage : window.sessionStorage;
 
-  if (!key) {
-    return [];
-  }
+	if (!key) {
+		return [];
+	}
 
-  const get = () => {
-    const valueSaved = storage.getItem(key);
-    if (!valueSaved) {
-      return null;
-    }
-    return JSON.parse(valueSaved);
-  };
+	const get = () => {
+		const valueSaved = storage.getItem(key);
+		if (!valueSaved) {
+			return null;
+		}
+		return JSON.parse(valueSaved);
+	};
 
-  const set = (rawValue) => {
-    const value = JSON.stringify(rawValue);
-    storage.setItem(key, value);
-  };
+	const set = (rawValue) => {
+		const value = JSON.stringify(rawValue);
+		storage.setItem(key, value);
+	};
 
-  const remove = () => {
-    storage.removeItem(key);
-  };
+	const remove = () => {
+		storage.removeItem(key);
+	};
 
-  return [get(), set, remove];
+	return [get(), set, remove];
 };
--- front-end/src/utils/Storage/keyNames.js
+++ front-end/src/utils/Storage/keyNames.js
@@ -1,1 +1,1 @@
-export const USER_INFO = "user_info";
+export const USER_INFO = 'user_info';
